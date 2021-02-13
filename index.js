4d2caa14f7f3f0d23916bc46e558e1f5f5ba6eeb
Kelvis Antonio Rodrigues Borges <kelvis.borges@ms.senac.br>
2021-02-13 12:14:15 -0400

    Cria api de seguros

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { salvarSeguro, listarSeguros } = require("./seguro-service");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+app.route('/api/seguros').post(salvarSeguro);
+app.route('/api/seguros').get(listarSeguros);
+
+const HOST = 'localhost';
+const PORT = 9000;
+
+const httpServer = app.listen(PORT, HOST, () => {
+    const msg = `Servidor rodando em http://${HOST}:${PORT}`;
+    console.log(msg);
+})
