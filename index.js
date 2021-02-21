ad471cf467c808c68498b1acaf8f88698f5a8b45
Kelvis Borges <kelvisbcc@gmail.com>
2021-02-21 12:56:41 -0300

    Adiciona envio de push notification

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const { enviarNotificacao } = require('./enviar-notificacao');
 const { salvarSeguro, listarSeguros } = require("./seguro-service");
+const { adicionaPushSubscriber } = require('./adiciona-push-subscriber');
+
+const webpush = require('web-push');
+
+
+
+// VAPID keys should only be generated only once.
+const vapidKeys = { 
+    publicKey: 'BFlGn5mEcLPW8Znn2SD7UDCiohSYHSMj-NnKUnDyOniNDY_2Y-ZKg_HcImrdq05vtGvp5f2wrq0bNQkL198f6YM',
+    privateKey: 'pLDLZh5bcaPyFqtTNUVdeGOw9ha7apD-B_uAAYgoo0M' 
+}
+
+webpush.setVapidDetails(
+    'mailto:kelvisbcc@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+)
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,8 +27,10 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.route('/api/seguros').post(salvarSeguro);
 app.route('/api/seguros').get(listarSeguros);
+app.route('/api/notificacao').post(adicionaPushSubscriber);
+app.route('/api/notificacao/enviar').post(enviarNotificacao);
 
-const HOST = 'localhost';
+const HOST = '127.0.0.1';
 const PORT = 9000;
 
 const httpServer = app.listen(PORT, HOST, () => {
